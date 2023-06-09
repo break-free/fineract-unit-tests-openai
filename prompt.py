775bda24db4b2c9629c34e0e185c9e2869d08738
Chris Mills <cmills@breakfreesolutions.com>
2023-06-09 09:31:24 -0500

    refactored prompt to use files generated by javalang

--- prompt.py
+++ prompt.py
@@ -1,32 +1,36 @@
 import faiss
-import langchain import OpenAI, LLMChain
-import langchain.prompts import Prompt
+import json
+from langchain import OpenAI, LLMChain
+from langchain.prompts import Prompt
 import pickle
 
 index = faiss.read_index("training.index")
-
 with open("faiss.pkl", "rb") as f:
     store = pickle.load(f)
-
 store.index = index
 
 with open("training/unit-test.prompt", "r") as f:
     promptTemplate = f.read()
-
-prompt = Prompt(template=promptTemplate, input_variables=["context", "question"])
-
+prompt = Prompt(template=promptTemplate, input_variables=["context", "question", "history"])
 llmChain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0.1))
 
-def onMessage(question, history):
-    docs = store.similarity_search(question)
+def onMessage(question):
+    chunks = store.similarity_search(question)
     contexts = []
-    for i, doc in enumerate(docs):
-        contexts.append(f"Context {i}:\n{doc.page_content}")
-        answer = llmChain.predict(question=question, 
-                                  context="\n\n".join(contexts))
-    return answer
+    for i, chunk in enumerate(chunks):
+        contexts.append(f"Context {i}:\n{chunk.page_content}")
+
+    with open('contexts.json', 'w') as f:
+        json.dump(contexts, f)
+    
+    return llmChain.predict(question=question, 
+                            context="\n\n".join(contexts),
+                            history="")
 
 while True:
     question = input("Ask a question > ")
-    answer = onMessage(question, history)
-    print(f"Bot: {answer}")
+    if question == 'exit':
+        break
+    else:
+        answer = onMessage(question)
+        print(f"Bot: {answer}")
