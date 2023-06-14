52a0b99d5aac175104f0c71eab9faffb8c630865
zgalford <zgalford@gmail.com>
2023-06-14 15:48:41 -0500

    making an arg to allow us to come in & see what context was sent

--- prompt.py
+++ prompt.py
@@ -20,7 +20,7 @@ with open("training/unit-test.prompt", "r") as f:
 prompt = Prompt(template=promptTemplate, input_variables=["context", "question", "history"])
 llmChain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0.1))
 
-def onMessage(question, history):
+def onMessage(question, history, show_context=False):
     # Retrieve chunks based on the question and assemble them into a 
     # joined context.
     chunks = store.similarity_search(question)
@@ -30,6 +30,9 @@ def onMessage(question, history):
     with open('contexts.json', 'w') as f:
         json.dump(contexts, f)
     joined_contexts = "\n\n".join(contexts)
+    if (show_context):
+        print(f"Context Provided: {joined_contexts}")
+
     # For each message to OpenAI, print tokens used for each part and in total
     question_tokens = llmChain.llm.get_num_tokens(question)
     contexts_tokens = llmChain.llm.get_num_tokens(joined_contexts)
@@ -45,11 +48,13 @@ def onMessage(question, history):
 
 history = ""
 while True:
+    if (sys.argv[1] == "Show_Context"):
+        show_context=True
     question = input("Ask a question > ")
     if question == 'exit':
         break
     else:
-        answer = onMessage(question, history)
+        answer = onMessage(question, history, show_context=show_context)
         history = history + answer +"\n\n###\n\n"
         print(f"Bot: {answer}")
         print("Answer tokens: " + str(llmChain.llm.get_num_tokens(answer)))
