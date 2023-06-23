0390ea7b06a927c0b329b3ff0d83243da3f895f1
Zak Alford <zalford@breakfreesolutions>
2023-06-23 16:47:37 -0500

    got the package installation to work locally, need to test it

--- src/llm_prompt_creator/chunker.py
+++ src/llm_prompt_creator/chunker.py
@@ -7,46 +7,44 @@ import sys
 Comb for specified file types. Currently only works for java files (*.java)
 Pulls out constants, constructors, fields, and methods and stores them as a Py dict in JSON file
 """
+if "OPENAI_API_KEY" not in os.environ:
+    print("You must set an OPENAI_API_KEY environment variable value", file=sys.stderr)
+    
 def chunker(directory, file_extension="*.java"):
-    if __name__ == '__main__':
-        # Check that environment variables are set up.
-        if "OPENAI_API_KEY" not in os.environ:
-            print("You must set an OPENAI_API_KEY environment variable value", file=sys.stderr)
-        # Retrieve file list
-        training_data = list()
-        if directory == None:
-            print("Must provide an input directory to chunk into tokens")
-        else:
-            training_data = JCC.get_file_list(directory, file_extension=file_extension)
-        # Chunk data using the files in the training data
-        chunks = []
-        failed_files = []
-        for file in training_data:
-            codelines = JCC.get_code_lines(file)
+    training_data = list()
+    # if directory == None:
+    #     print("Must provide an input directory to chunk into tokens")
+    
+    training_data = JCC.get_file_list(directory, file_extension=file_extension)
+    # Chunk data using the files in the training data
+    chunks = []
+    failed_files = []
+    for file in training_data:
+        codelines = JCC.get_code_lines(file)
+        try:
+            tree = JCC.parse_code(file, codelines)
+        except JCC.ParseError as e:
+            failed_files.append(str(file) + ": " + str(e))
+        if tree != None:
             try:
-                tree = JCC.parse_code(file, codelines)
-            except JCC.ParseError as e:
+                chunks = chunks + JCC.chunk_constants(tree)
+                chunks = chunks + JCC.chunk_constructors(tree, codelines)
+                chunks = chunks + JCC.chunk_fields(tree, codelines)
+                chunks = chunks + JCC.chunk_methods(tree, codelines)
+            except JCC.ChunkingError as e:
                 failed_files.append(str(file) + ": " + str(e))
-            if tree != None:
-                try:
-                    chunks = chunks + JCC.chunk_constants(tree)
-                    chunks = chunks + JCC.chunk_constructors(tree, codelines)
-                    chunks = chunks + JCC.chunk_fields(tree, codelines)
-                    chunks = chunks + JCC.chunk_methods(tree, codelines)
-                except JCC.ChunkingError as e:
-                    failed_files.append(str(file) + ": " + str(e))
-            else:
-                failed_files.append(str(file) + ", has no tree!")
-        
-        # Convert training_data paths into strings for serialization.
-        training_data_str = list()
-        for data in training_data: 
-            training_data_str.append(str(data))
-        # Save each used list as a file for other operations.
-        with open('training_data.json', 'w') as f:
-            json.dump(training_data_str, f)
-        with open('chunks.json', 'w') as f:
-            json.dump(chunks, f)
-        with open('failed_files.json', 'w') as f:
-            json.dump(failed_files, f)
+        else:
+            failed_files.append(str(file) + ", has no tree!")
+    
+    # Convert training_data paths into strings for serialization.
+    training_data_str = list()
+    for data in training_data: 
+        training_data_str.append(str(data))
+    # Save each used list as a file for other operations.
+    with open('training_data.json', 'w') as f:
+        json.dump(training_data_str, f)
+    with open('chunks.json', 'w') as f:
+        json.dump(chunks, f)
+    with open('failed_files.json', 'w') as f:
+        json.dump(failed_files, f)
 
